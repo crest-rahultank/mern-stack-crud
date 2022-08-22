bf94487faa4ca7f4de7de1de4f60a6137fd517b2
Rahul Tank <rahul.t@crestinfosystems.net>
2022-08-22 13:19:06 +0530

    MERN first project initial commit.

---
+++ src/App.js
@@ -0,0 +1,76 @@
+// Import React
+import React from "react";
+
+// Import Bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+
+// Import Custom CSS
+import "./App.css";
+
+// Import from react-router-dom
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
+// Import other React Component
+import CreateStudent from "./Components/create-student.component";
+import EditStudent from "./Components/edit-student.component";
+import StudentList from "./Components/student-list.component";
+
+// App Component
+const App = () => {
+return (
+	<Router>
+	<div className="App">
+		<header className="App-header">
+		<Navbar bg="dark" variant="dark">
+			<Container>
+			<Navbar.Brand>
+				<Link to={"/create-student"}
+				className="nav-link">
+				React MERN Stack App
+				</Link>
+			</Navbar.Brand>
+
+			<Nav className="justify-content-end">
+				<Nav>
+				<Link to={"/create-student"}
+					className="nav-link">
+					Create Student
+				</Link>
+				</Nav>
+
+				<Nav>
+				<Link to={"/student-list"}
+					className="nav-link">
+					Student List
+				</Link>
+				</Nav>
+			</Nav>
+			</Container>
+		</Navbar>
+		</header>
+
+		<Container>
+		<Row>
+			<Col md={12}>
+			<div className="wrapper">
+				<Routes>
+				<Route exact path="/"
+					component={CreateStudent} />
+				<Route path="/create-student"
+					component={CreateStudent} />
+				<Route path="/edit-student/:id"
+					component={EditStudent} />
+				<Route path="/student-list"
+					component={StudentList} />
+				</Routes>
+			</div>
+			</Col>
+		</Row>
+		</Container>
+	</div>
+	</Router>
+);
+};
+
+export default App;
---
+++ src/Components/StudentForm.js
@@ -0,0 +1,59 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React from "react";
+import { Button, FormGroup } from "react-bootstrap";
+import * as Yup from "yup";
+
+const StudentForm = (props) => {
+const validationSchema = Yup.object().shape({
+	name: Yup.string().required("Required"),
+	email: Yup.string()
+	.email("You have enter an invalid email address")
+	.required("Required"),
+	rollno: Yup.number()
+	.positive("Invalid roll number")
+	.integer("Invalid roll number")
+	.required("Required"),
+});
+console.log(props);
+return (
+	<div className="form-wrapper">
+	<Formik {...props} validationSchema={validationSchema}>
+		<Form>
+		<FormGroup>
+			<Field name="name" type="text"
+				className="form-control" />
+			<ErrorMessage
+			name="name"
+			className="d-block invalid-feedback"
+			component="span"
+			/>
+		</FormGroup>
+		<FormGroup>
+			<Field name="email" type="text"
+				className="form-control" />
+			<ErrorMessage
+			name="email"
+			className="d-block invalid-feedback"
+			component="span"
+			/>
+		</FormGroup>
+		<FormGroup>
+			<Field name="rollno" type="number"
+				className="form-control" />
+			<ErrorMessage
+			name="rollno"
+			className="d-block invalid-feedback"
+			component="span"
+			/>
+		</FormGroup>
+		<Button variant="danger" size="lg"
+			block="block" type="submit">
+			{props.children}
+		</Button>
+		</Form>
+	</Formik>
+	</div>
+);
+};
+
+export default StudentForm;
